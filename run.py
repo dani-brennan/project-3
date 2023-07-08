3feb53cf09cc70b9fe5116b48c7eaa0e3e0ebb0d
dani-brennan <danielle.brennan95@gmail.com>
2023-07-08 15:57:12 +0000

    Add welcome message and game instructions inputs

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 # welcome message
-
+print("Welcome to my Black Jack Game!")
 # instructions
+
+user_input = input("Have you ever played Black Jack before? (Y/N):")
+
+if user_input.lower() == "y":
+    print("Great! Let's begin.")
+
+elif user_input.lower() == "n":
+    print("Game Instructions")
+else:
+    print('Please type Y or N')
 
 # deck of cards
 
