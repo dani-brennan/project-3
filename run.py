8628093db6539ea8f684176fbedf320391919ffc
dani-brennan <danielle.brennan95@gmail.com>
2023-07-08 20:52:51 +0000

    Add if statements to change value of ace depending on the players total value of cards

--- run.py
+++ run.py
@@ -80,7 +80,15 @@ class PlayerHand:
 
     def compute_value(self):
         self.value = 0
+        holding_ace = False
         for card in self.cards:
             card_value = int(card.rank["value"])
             self.value += card_value
+            if card.rank["rank"] =="A":
+                holding_ace = True
+                
+        # If the player is holding an ace and the total value would be over 21, 
+        # Ace will equal 1 instead
+        if holding_ace and self.value > 21:
+            self.value -= 10
             
