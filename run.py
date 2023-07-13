4ab91032ba42df1a947ea809e23ceceb8bebc53b
dani-brennan <danielle.brennan95@gmail.com>
2023-07-13 21:28:46 +0000

    Fix  whitespace, lines that were too long

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ win_score = 0
 loss_score = 0
 tie_score = 0
 
+
 def print_slower(input_str):
     for c in input_str:
         sys.stdout.write(c)
@@ -13,40 +14,45 @@ def print_slower(input_str):
         time.sleep(0.08)
     sys.stdout.write('\n')
 
+
 # welcome message
 def game_intro():
     print("<>" * 12)
     #print_slower("WELCOME TO BLACKJACK!")
     print("<>" * 12)
     print()
-    #print_slower("Dealer: I don't think I've seen you around here before... What's your name?")
+    #print_slower("Dealer: I don't think I've seen you around here \
+    #before... What's your name?")
     x = input()
     print("-~" * 12)
     #print_slower("Dealer: Welcome to my table " + x + " :)")
     print("-~" * 12)
 
     print()
-    userAnswer = None 
+    userAnswer = None
 
-    while userAnswer not in ("y", "n"): 
+    while userAnswer not in ("y", "n"):
         userAnswer = input("Dealer: Need me to teach you how to play? ")
 
-        if userAnswer == "y": 
+        if userAnswer == "y":
             # Show the player how to play
             print_slower("Okay, the rules of Blackjack are..")
             print("--" * 12)
             print_slower("Each player gets dealt 2 cards.")
-            print_slower("You can then decide whether to Hit (get another card) or Stand. ")
-            print_slower("To win you must have a higher hand value than me, which must not exceed 21.")
+            print_slower("You can then decide whether to Hit \
+            (get another card) or Stand. ")
+            print_slower("To win you must have a higher hand value than me, \
+            which must not exceed 21.")
             print_slower("You'll also win if I have a hand value over 21.")
-            print_slower("If the hand value is worth 21.. you have Blackjack, which wins the game.")
+            print_slower("If the hand value is worth 21.. \
+            you have Blackjack, which wins the game.")
             print_slower("If the hand value is over 21, player busts.")
             print("--" * 12)
             ready_to_play = False
 
             while ready_to_play == False:
                 ready_to_play = input("Type 'start' to begin: ")
-                
+
                 if ready_to_play == str("start" or "Start"):
                     ready_to_play = True
                     # Start Game
@@ -54,19 +60,22 @@ def game_intro():
                 else:
                     print("" * 12)
                     ready_to_play = False
-                        
-        elif userAnswer == "n": 
+
+        elif userAnswer == "n":
             # Start Game
             print_slower("The dealer is shuffling the deck...")
-        else: 
-            print("Please enter yes or no: ") 
+        else:
+            print("Please enter yes or no: ")
+
 
 game_intro()
+
 
 class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
     def __str__(self):
         return f'> {self.rank["rank"]} of {self.suit}'
 
@@ -88,7 +97,8 @@ class DeckofCards:
                 {"rank": "8", "value": "8"},
                 {"rank": "9", "value": "9"},
                 {"rank": "10", "value": "10"},
-                # If the player is dealt a Jack(J), Queen(Q) or King(K), it's value will be 10
+                # If the player is dealt a Jack(J), Queen(Q)
+                # or King(K), it's value will be 10
                 {"rank": "K", "value": "10"},
                 {"rank": "Q", "value": "10"},
                 {"rank": "J", "value": "10"},
@@ -116,8 +126,9 @@ class DeckofCards:
                 cards_dealt.append(card)
         return cards_dealt
 
+
 class PlayerHand:
-    def __init__(self, dealer = False):
+    def __init__(self, dealer=False):
         self.cards = []
         self.value = 0
         self.dealer = dealer
@@ -131,10 +142,10 @@ class PlayerHand:
         for card in self.cards:
             card_value = int(card.rank["value"])
             self.value += card_value
-            if card.rank["rank"] =="A":
+            if card.rank["rank"] == "A":
                 holding_ace = True
 
-        # If the player is holding an ace and the total value would be over 21, 
+        # If the player is holding an ace and the total value would be over 21,
         # Ace will equal 1 instead
         if holding_ace and self.value > 21:
             self.value -= 10
@@ -146,23 +157,23 @@ class PlayerHand:
     def black_jack(self):
         return self.find_value() == 21
 
-    def view_cards(self, show_dealers_hand = False):
+    def view_cards(self, show_dealers_hand=False):
 
-        print(f'''{"Dealer" if self.dealer else "Your"} hand:''')   
+        print(f'''{"Dealer" if self.dealer else "Your"} hand:''')
 
         for index, card in enumerate(self.cards):
 
             # Make dealers hand hidden
-            if index == 0 and self.dealer and not show_dealers_hand and not self.black_jack():
+            if index == 0 and self.dealer and not show_dealers_hand \
+            and not self.black_jack():
                 print("hidden")
-            else: 
+            else:
                 print(card)
 
         # If the player is not the dealer
         if not self.dealer:
             print("Total value:", self.find_value())
             print()
-
 
 
 class Game:
@@ -174,19 +185,13 @@ class Game:
         elif result == "loss":
             loss_score += 1
         else:
-            tie_score += 1
+            loss_score += 10000
 
     def play(self):
         game_number = 3
         play_again = True
         total_games_played = 0
         print("Playing 3 rounds of Blackjack")
-
-        while play_again == True:
-            try:
-                play_again = input("Press Enter to continue...")
-            except:
-                print("Please press enter to continue.")
 
         while game_number > 0:
             game_number -= 1
@@ -196,12 +201,12 @@ class Game:
             deck.shuffle()
 
             players_hand = PlayerHand()
-            dealers_hand = PlayerHand(dealer = True)
+            dealers_hand = PlayerHand(dealer=True)
 
             for i in range(2):
                 players_hand.attach_card(deck.dealCards(1))
                 dealers_hand.attach_card(deck.dealCards(1))
-            
+
             print("~" * 6)
 
             players_hand.view_cards()
@@ -209,11 +214,12 @@ class Game:
 
             if self.check_for_win(players_hand, dealers_hand):
                 continue
-            
+
             hit_or_stand = ""
-            # While the players hand is less than 21, give the player the choice
-            # to hit or stand
-            while players_hand.find_value() < 21 and hit_or_stand not in ["Stand", "stand"]:
+            # While the players hand is less than 21, give the
+            # player the choice to hit or stand
+            while players_hand.find_value() < 21 and hit_or_stand \
+            not in ["Stand", "stand"]:
                 hit_or_stand = input("Would you like to 'Hit' or 'Stand'?: ")
 
                 while hit_or_stand not in ["Hit", "hit", "Stand", "stand"]:
@@ -225,7 +231,7 @@ class Game:
 
             if self.check_for_win(players_hand, dealers_hand):
                 continue
-            
+
             players_hand_total = players_hand.find_value()
             dealers_hand_total = dealers_hand.find_value()
 
@@ -236,14 +242,13 @@ class Game:
             dealers_hand.view_cards(show_dealers_hand=True)
             if self.check_for_win(players_hand, dealers_hand):
                 continue
-            
+
             print_slower("Game Results: ")
             print_slower("Your Hand: ")
             print(players_hand_total)
             print_slower("Dealer's Hand: ")
             print(dealers_hand_total)
-            result = self.score_update()
-            score_update(result)
+            result = check_for_win(self, players_hand, dealers_hand)
             print(result)
             print(f"Out of {total_games_played} Games you won: {win_score}, lost {loss_score} and tied {tie_score}!")
 
@@ -251,9 +256,9 @@ class Game:
 
         print(f"Out of {total_games_played} You won: {win_score}, lost: {loss_score} and tied {tie_score}")
 
-    def check_for_win(self, players_hand, dealers_hand, game_over = False, player_win = False):
+    def check_for_win(self, players_hand, dealers_hand, game_over=False, player_win=False):
         if not game_over:
-            
+
             # If the total value of the players cards is over 21, player loses
             if players_hand.find_value() > 21:
                 print_slower("Bust! Dealer wins.")
@@ -264,13 +269,13 @@ class Game:
                 print_slower("You win! Dealer has bust.")
                 return "win"
                 return True
-            
+
             # If player has a total card value of 21, player wins
             elif players_hand.black_jack():
                 print_slower("Black Jack! You win!")
                 return "win"
                 return True
-            
+
             # If dealer has a total card value of 21, dealer wins
             elif dealers_hand.black_jack():
                 print_slower("You lose! Dealer has Black Jack.")
@@ -280,7 +285,7 @@ class Game:
             elif dealers_hand.black_jack() and players_hand.black_jack():
                 print_slower("It's a tie! You both have Black Jack.")
                 return "tie"
-                return True    
+                return True
         else:
             # If the players card value is more that the dealers card value
             if players_hand.find_value() > dealers_hand.find_value():
@@ -298,7 +303,5 @@ class Game:
         return False
 
 
-
 game = Game()
 game.play()
-
